37b3dee92cde73ed9b4bf38c265721e70b7c1e87
Eirik Stensen <eirikkst@stud.ntnu.no>
2017-10-11 17:09:06 +0200

    Bestillingside

---
+++ src/main/webapp/dateTime.js
@@ -0,0 +1,17 @@
+/*
+Timepicker
+ */
+
+$(function () {
+    $('#datetimepicker1').datetimepicker();
+});
+
+/*
+Clock picker
+ */
+$('.clockpicker').clockpicker({
+    placement: 'top',
+    align: 'left',
+    donetext: 'Done'
+});
+
